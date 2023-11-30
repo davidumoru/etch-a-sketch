f5dc606f5a09464ed48a902bba9737a194f6d70e
David Umoru <umorudavido@gmail.com>
2023-11-30 02:41:39 +0100

    Add event listener to change grid color on hover

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const container = document.querySelector("#container");
 
+const changeColor = (element) => {
+  element.style.backgroundColor = "black";
+};
+
 for (let x = 0; x < 16; x++) {
   for (let j = 0; j < 16; j++) {
     const grid = document.createElement("div");
     grid.className = "grid-item";
+    grid.addEventListener("mouseover", () => changeColor(grid));
     container.appendChild(grid);
   }
 }
